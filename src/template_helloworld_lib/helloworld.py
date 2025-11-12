6563160c201a2db7192ceee2fa1b56e690e500c9
Ioannis Krommydas <krommydas@cpvo.europa.eu>
2025-11-12 23:37:12 +0100

    Fix test case.

--- src/template_helloworld_lib/helloworld.py
+++ src/template_helloworld_lib/helloworld.py
@@ -3,6 +3,6 @@ from typing import Optional
 
 def tell_me_hello_world(name : Optional[str] = None) -> str:
     if name:
-        return f"Hallo World, {name}!"
+        return f"Hello World, {name}!"
     return "Hello World!"
 
